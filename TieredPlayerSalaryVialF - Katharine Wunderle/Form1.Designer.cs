734c017e805fc003f40cf7f6b9f291640892e33f
Katie <95194045+kwunderle@users.noreply.github.com>
2022-08-16 22:52:22 -0400

    Add files via upload

---
+++ TieredPlayerSalaryVialF - Katharine Wunderle/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace TieredPlayerSalaryVialF___Katharine_Wunderle
+{
+    partial class salaryCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerInfoLbl = new System.Windows.Forms.Label();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.hitNoLbl = new System.Windows.Forms.Label();
+            this.nameInpt = new System.Windows.Forms.TextBox();
+            this.hitInpt = new System.Windows.Forms.TextBox();
+            this.allStarChkBx = new System.Windows.Forms.CheckBox();
+            this.mvpChkBx = new System.Windows.Forms.CheckBox();
+            this.tierLbl = new System.Windows.Forms.Label();
+            this.salaryLbl = new System.Windows.Forms.Label();
+            this.tierOtpt = new System.Windows.Forms.Label();
+            this.salaryOtpt = new System.Windows.Forms.Label();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.nameOtptLbl = new System.Windows.Forms.Label();
+            this.leadingPlayerLbl = new System.Windows.Forms.Label();
+            this.leadingNameLbl = new System.Windows.Forms.Label();
+            this.leadingHitsLbl = new System.Windows.Forms.Label();
+            this.leadingSalaryLbl = new System.Windows.Forms.Label();
+            this.leadingTierLbl = new System.Windows.Forms.Label();
+            this.leadName = new System.Windows.Forms.Label();
+            this.leadHit = new System.Windows.Forms.Label();
+            this.leadSalary = new System.Windows.Forms.Label();
+            this.leadTier = new System.Windows.Forms.Label();
+            this.clrBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playerInfoLbl
+            // 
+            this.playerInfoLbl.AutoSize = true;
+            this.playerInfoLbl.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerInfoLbl.Location = new System.Drawing.Point(45, 65);
+            this.playerInfoLbl.Name = "playerInfoLbl";
+            this.playerInfoLbl.Size = new System.Drawing.Size(167, 16);
+            this.playerInfoLbl.TabIndex = 0;
+            this.playerInfoLbl.Text = "Enter Player Information:";
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(57, 91);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(38, 13);
+            this.nameLbl.TabIndex = 1;
+            this.nameLbl.Text = "Name:";
+            // 
+            // hitNoLbl
+            // 
+            this.hitNoLbl.AutoSize = true;
+            this.hitNoLbl.Location = new System.Drawing.Point(15, 117);
+            this.hitNoLbl.Name = "hitNoLbl";
+            this.hitNoLbl.Size = new System.Drawing.Size(80, 13);
+            this.hitNoLbl.TabIndex = 2;
+            this.hitNoLbl.Text = "Number of Hits:";
+            // 
+            // nameInpt
+            // 
+            this.nameInpt.Location = new System.Drawing.Point(101, 88);
+            this.nameInpt.Name = "nameInpt";
+            this.nameInpt.Size = new System.Drawing.Size(100, 20);
+            this.nameInpt.TabIndex = 3;
+            // 
+            // hitInpt
+            // 
+            this.hitInpt.Location = new System.Drawing.Point(101, 114);
+            this.hitInpt.Name = "hitInpt";
+            this.hitInpt.Size = new System.Drawing.Size(100, 20);
+            this.hitInpt.TabIndex = 4;
+            // 
+            // allStarChkBx
+            // 
+            this.allStarChkBx.AutoSize = true;
+            this.allStarChkBx.Location = new System.Drawing.Point(101, 139);
+            this.allStarChkBx.Name = "allStarChkBx";
+            this.allStarChkBx.Size = new System.Drawing.Size(59, 17);
+            this.allStarChkBx.TabIndex = 5;
+            this.allStarChkBx.Text = "All Star";
+            this.allStarChkBx.UseVisualStyleBackColor = true;
+            // 
+            // mvpChkBx
+            // 
+            this.mvpChkBx.AutoSize = true;
+            this.mvpChkBx.Location = new System.Drawing.Point(101, 162);
+            this.mvpChkBx.Name = "mvpChkBx";
+            this.mvpChkBx.Size = new System.Drawing.Size(49, 17);
+            this.mvpChkBx.TabIndex = 6;
+            this.mvpChkBx.Text = "MVP";
+            this.mvpChkBx.UseVisualStyleBackColor = true;
+            // 
+            // tierLbl
+            // 
+            this.tierLbl.AutoSize = true;
+            this.tierLbl.Location = new System.Drawing.Point(35, 263);
+            this.tierLbl.Name = "tierLbl";
+            this.tierLbl.Size = new System.Drawing.Size(60, 13);
+            this.tierLbl.TabIndex = 7;
+            this.tierLbl.Text = "Player Tier:";
+            // 
+            // salaryLbl
+            // 
+            this.salaryLbl.AutoSize = true;
+            this.salaryLbl.Location = new System.Drawing.Point(24, 293);
+            this.salaryLbl.Name = "salaryLbl";
+            this.salaryLbl.Size = new System.Drawing.Size(71, 13);
+            this.salaryLbl.TabIndex = 8;
+            this.salaryLbl.Text = "Player Salary:";
+            // 
+            // tierOtpt
+            // 
+            this.tierOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tierOtpt.Location = new System.Drawing.Point(101, 258);
+            this.tierOtpt.Name = "tierOtpt";
+            this.tierOtpt.Size = new System.Drawing.Size(100, 23);
+            this.tierOtpt.TabIndex = 9;
+            this.tierOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // salaryOtpt
+            // 
+            this.salaryOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.salaryOtpt.Location = new System.Drawing.Point(101, 288);
+            this.salaryOtpt.Name = "salaryOtpt";
+            this.salaryOtpt.Size = new System.Drawing.Size(100, 23);
+            this.salaryOtpt.TabIndex = 10;
+            this.salaryOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(101, 185);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcBtn.TabIndex = 11;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // nameOtptLbl
+            // 
+            this.nameOtptLbl.AutoSize = true;
+            this.nameOtptLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameOtptLbl.Location = new System.Drawing.Point(83, 230);
+            this.nameOtptLbl.Name = "nameOtptLbl";
+            this.nameOtptLbl.Size = new System.Drawing.Size(73, 13);
+            this.nameOtptLbl.TabIndex = 12;
+            this.nameOtptLbl.Text = "Current Player";
+            // 
+            // leadingPlayerLbl
+            // 
+            this.leadingPlayerLbl.AutoSize = true;
+            this.leadingPlayerLbl.Font = new System.Drawing.Font("Century Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leadingPlayerLbl.Location = new System.Drawing.Point(265, 65);
+            this.leadingPlayerLbl.Name = "leadingPlayerLbl";
+            this.leadingPlayerLbl.Size = new System.Drawing.Size(106, 16);
+            this.leadingPlayerLbl.TabIndex = 13;
+            this.leadingPlayerLbl.Text = "Leading Player";
+            // 
+            // leadingNameLbl
+            // 
+            this.leadingNameLbl.AutoSize = true;
+            this.leadingNameLbl.Location = new System.Drawing.Point(254, 90);
+            this.leadingNameLbl.Name = "leadingNameLbl";
+            this.leadingNameLbl.Size = new System.Drawing.Size(38, 13);
+            this.leadingNameLbl.TabIndex = 14;
+            this.leadingNameLbl.Text = "Name:";
+            // 
+            // leadingHitsLbl
+            // 
+            this.leadingHitsLbl.AutoSize = true;
+            this.leadingHitsLbl.Location = new System.Drawing.Point(265, 122);
+            this.leadingHitsLbl.Name = "leadingHitsLbl";
+            this.leadingHitsLbl.Size = new System.Drawing.Size(28, 13);
+            this.leadingHitsLbl.TabIndex = 15;
+            this.leadingHitsLbl.Text = "Hits:";
+            // 
+            // leadingSalaryLbl
+            // 
+            this.leadingSalaryLbl.AutoSize = true;
+            this.leadingSalaryLbl.Location = new System.Drawing.Point(254, 154);
+            this.leadingSalaryLbl.Name = "leadingSalaryLbl";
+            this.leadingSalaryLbl.Size = new System.Drawing.Size(39, 13);
+            this.leadingSalaryLbl.TabIndex = 16;
+            this.leadingSalaryLbl.Text = "Salary:";
+            // 
+            // leadingTierLbl
+            // 
+            this.leadingTierLbl.AutoSize = true;
+            this.leadingTierLbl.Location = new System.Drawing.Point(265, 188);
+            this.leadingTierLbl.Name = "leadingTierLbl";
+            this.leadingTierLbl.Size = new System.Drawing.Size(28, 13);
+            this.leadingTierLbl.TabIndex = 17;
+            this.leadingTierLbl.Text = "Tier:";
+            // 
+            // leadName
+            // 
+            this.leadName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.leadName.Location = new System.Drawing.Point(298, 85);
+            this.leadName.Name = "leadName";
+            this.leadName.Size = new System.Drawing.Size(100, 23);
+            this.leadName.TabIndex = 18;
+            this.leadName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // leadHit
+            // 
+            this.leadHit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.leadHit.Location = new System.Drawing.Point(298, 117);
+            this.leadHit.Name = "leadHit";
+            this.leadHit.Size = new System.Drawing.Size(100, 23);
+            this.leadHit.TabIndex = 19;
+            this.leadHit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // leadSalary
+            // 
+            this.leadSalary.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.leadSalary.Location = new System.Drawing.Point(299, 149);
+            this.leadSalary.Name = "leadSalary";
+            this.leadSalary.Size = new System.Drawing.Size(100, 23);
+            this.leadSalary.TabIndex = 20;
+            this.leadSalary.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // leadTier
+            // 
+            this.leadTier.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.leadTier.Location = new System.Drawing.Point(299, 183);
+            this.leadTier.Name = "leadTier";
+            this.leadTier.Size = new System.Drawing.Size(100, 23);
+            this.leadTier.TabIndex = 21;
+            this.leadTier.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // clrBtn
+            // 
+            this.clrBtn.Location = new System.Drawing.Point(296, 263);
+            this.clrBtn.Name = "clrBtn";
+            this.clrBtn.Size = new System.Drawing.Size(75, 23);
+            this.clrBtn.TabIndex = 22;
+            this.clrBtn.Text = "Clear";
+            this.clrBtn.UseVisualStyleBackColor = true;
+            this.clrBtn.Click += new System.EventHandler(this.clrBtn_Click);
+            // 
+            // salaryCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 331);
+            this.Controls.Add(this.clrBtn);
+            this.Controls.Add(this.leadTier);
+            this.Controls.Add(this.leadSalary);
+            this.Controls.Add(this.leadHit);
+            this.Controls.Add(this.leadName);
+            this.Controls.Add(this.leadingTierLbl);
+            this.Controls.Add(this.leadingSalaryLbl);
+            this.Controls.Add(this.leadingHitsLbl);
+            this.Controls.Add(this.leadingNameLbl);
+            this.Controls.Add(this.leadingPlayerLbl);
+            this.Controls.Add(this.nameOtptLbl);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.salaryOtpt);
+            this.Controls.Add(this.tierOtpt);
+            this.Controls.Add(this.salaryLbl);
+            this.Controls.Add(this.tierLbl);
+            this.Controls.Add(this.mvpChkBx);
+            this.Controls.Add(this.allStarChkBx);
+            this.Controls.Add(this.hitInpt);
+            this.Controls.Add(this.nameInpt);
+            this.Controls.Add(this.hitNoLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.playerInfoLbl);
+            this.Name = "salaryCalc";
+            this.Text = "Tiered Player Salary Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label playerInfoLbl;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label hitNoLbl;
+        private System.Windows.Forms.TextBox nameInpt;
+        private System.Windows.Forms.TextBox hitInpt;
+        private System.Windows.Forms.CheckBox allStarChkBx;
+        private System.Windows.Forms.CheckBox mvpChkBx;
+        private System.Windows.Forms.Label tierLbl;
+        private System.Windows.Forms.Label salaryLbl;
+        private System.Windows.Forms.Label tierOtpt;
+        private System.Windows.Forms.Label salaryOtpt;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label nameOtptLbl;
+        private System.Windows.Forms.Label leadingPlayerLbl;
+        private System.Windows.Forms.Label leadingNameLbl;
+        private System.Windows.Forms.Label leadingHitsLbl;
+        private System.Windows.Forms.Label leadingSalaryLbl;
+        private System.Windows.Forms.Label leadingTierLbl;
+        private System.Windows.Forms.Label leadName;
+        private System.Windows.Forms.Label leadHit;
+        private System.Windows.Forms.Label leadSalary;
+        private System.Windows.Forms.Label leadTier;
+        private System.Windows.Forms.Button clrBtn;
+    }
+}
+
---
+++ TieredPlayerSalaryVialF - Katharine Wunderle/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TieredPlayerSalaryVialF___Katharine_Wunderle
+{
+    //Author: Katharine Wunderle
+    //ID: 623748
+    //Date: 03/06/2021
+    //Goal: Calculate players tier and salary based on number of hits
+    public partial class salaryCalc : Form
+    {
+        public salaryCalc()
+        {
+            InitializeComponent();
+        }
+        //Tier Constants
+        const decimal tier1 = 17500m;
+        const decimal tier2 = 20000m;
+        const decimal tier3 = 22500m;
+        const decimal tier4 = 25000m;
+        //Declare variables
+        //Name label variable
+        string name;
+        //Input variable
+        decimal hitNo = 0;
+        //Output variables
+        decimal salary = 0m;
+        decimal tier = 0;
+        bool tryParseResultBool;
+        //Highest hit variable
+        decimal highScore = 0;
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+
+            //INPUT
+            //Receive and verify player name input by user
+            name = nameInpt.Text;
+            if (nameInpt.Text == "")
+            {
+                MessageBox.Show("Please enter Player's Name.");
+                return;
+            }
+            //Receive and verify hit count input from user
+            tryParseResultBool = decimal.TryParse(hitInpt.Text, out hitNo);
+            if (tryParseResultBool == false || hitNo <= 0)
+            {
+                MessageBox.Show("Error: Please enter a positive numeric value");
+                return;
+            }
+            else if (hitNo > 300)
+            {
+                MessageBox.Show("Error: Too Many Hits");
+                return;
+            }
+            //Calculate and display tier based on hit number
+            if (hitNo <= 49)
+            {
+                tier = tier1;
+                tierOtpt.Text = "1";
+            }
+            if (hitNo >= 50)
+            {
+                tier = tier2;
+                tierOtpt.Text = "2";
+            }
+
+            if (hitNo >= 100)
+            {
+                tier = tier3;
+                tierOtpt.Text = "3";
+            }
+
+            if (hitNo >= 150)
+            {
+                tier = tier4;
+                tierOtpt.Text = "4";
+            }
+
+            //Calculate salary
+            salary = hitNo * tier;
+            //Add 25% if both mvp and all star boxes checked
+            if (allStarChkBx.Checked && mvpChkBx.Checked)
+            {
+                salary += salary * .25m;
+            }
+            //Add 20% to salary if either mvp or all star is checked
+            else if (allStarChkBx.Checked || mvpChkBx.Checked)
+            {
+                salary += salary * .20m;
+            }
+            //Calculate highest hit
+            if (hitNo > highScore)
+            {
+                //Display leading player information
+                highScore = hitNo;
+                leadName.Text = name;
+                leadHit.Text = hitNo.ToString();
+                leadSalary.Text = salary.ToString("c");
+                leadTier.Text = tierOtpt.Text;
+            }
+            //Display current player
+            nameOtptLbl.Text = name;
+            //Display current player's salary
+            salaryOtpt.Text = salary.ToString("c");
+        }
+
+        private void clrBtn_Click(object sender, EventArgs e)
+        {
+            //Clear input text boxes
+            nameInpt.Text = "";
+            hitInpt.Text = "";
+            //Clear checkboxes if checked
+            allStarChkBx.Checked = false;
+            mvpChkBx.Checked = false;
+            //Clear currentn player name and info
+            nameOtptLbl.Text = "Current Player";
+            tierOtpt.Text = "";
+            salaryOtpt.Text = "";
+            //Clear leading player info
+            leadName.Text = "";
+            leadHit.Text = "";
+            leadSalary.Text = "";
+            leadTier.Text = "";
+            //Reset high score variable
+            highScore = 0;
+        }
+    }
+}
